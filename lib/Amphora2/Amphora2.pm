e5ed67493db24877df7fd849e78f9e2d33cedda5
koadman <aarondarling@ucdavis.edu>
2011-04-15 10:37:17 -0700

    adding infrastructure for a perl module

---
+++ lib/Amphora2/Amphora2.pm
@@ -0,0 +1,111 @@
+package Amphora2::Amphora2;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Amphora2::Amphora2 - The great new Amphora2::Amphora2!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Amphora2::Amphora2;
+
+    my $foo = Amphora2::Amphora2->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Aaron Darling, C<< <aarondarling at ucdavis.edu> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-amphora2-amphora2 at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Amphora2-Amphora2>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Amphora2::Amphora2
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Amphora2-Amphora2>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Amphora2-Amphora2>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Amphora2-Amphora2>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Amphora2-Amphora2/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Aaron Darling.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Amphora2::Amphora2
