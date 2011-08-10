1d48b9c4a4f3411902435b3d3fc3526b58ebe78c
koadman <koadman@koadmans-MacBook-Air.local>
2011-08-10 07:48:15 -0700

    typo fix for os detect

--- lib/Amphora2/Utilities.pm
+++ lib/Amphora2/Utilities.pm
@@ -83,7 +83,12 @@ sub get_program_path {
 	}
 	# last ditch attempt, check the directories from where the script is running
 	$progcheck = $Bin."/".$progname unless( $progcheck =~ /$progname/ || !(-x $Bin."/".$progname) );
-	$progcheck = $Bin."/bin/".$progname unless( $progcheck =~ /$progname/  && !(-x $Bin."/".$progname) );
+	# check the OS and use Mac binaries if needed
+	if($^O =~ /arwin/){
+		$progcheck = $Bin."/osx/".$progname unless( $progcheck =~ /$progname/  && !(-x $Bin."/".$progname) );
+	}else{
+		$progcheck = $Bin."/bin/".$progname unless( $progcheck =~ /$progname/  && !(-x $Bin."/".$progname) );
+	}
 	return $progcheck;
 }
 
