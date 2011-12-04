4ffdb455ef97603b7ff45fdcc5b765836eafe5bb
koadman <aarondarling@ucdavis.edu>
2011-12-04 06:01:34 -0800

    eliminate Math::Random

--- tools/package_standalone.pl
+++ tools/package_standalone.pl
@@ -10,14 +10,6 @@ use warnings;
 `rm -rf Amphora-2/.git`;
 `git clone git://github.com/bioperl/bioperl-live.git`;
 `mv bioperl-live/Bio* Amphora-2/lib`;
-`wget http://search.cpan.org/CPAN/authors/id/G/GR/GROMMEL/Math-Random-0.71.tar.gz`;
-`tar xvzf Math-Random-0.71.tar.gz`;
-chdir("Math-Random-0.71");
-`perl Makefile.PL`;
-`make`;
-`mv blib/arch/auto ../Amphora-2/lib/`;
-`mv blib/lib/Math ../Amphora-2/lib/`;
-chdir("..");
 
 `wget http://search.cpan.org/CPAN/authors/id/R/RV/RVOSA/Bio-Phylo-0.45.tar.gz`;
 `tar xvzf Bio-Phylo-0.45.tar.gz`;
