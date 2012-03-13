120ec113f5c6c11780837922085fadb2e9b1b305
gjospin <gjospin@ucdavis.edu>
2012-03-13 14:21:35 -0700

    fixed taxon_name and taxon_level issues

--- lib/Phylosift/Summarize.pm
+++ lib/Phylosift/Summarize.pm
@@ -320,11 +320,15 @@ sub summarize {
 		foreach my $taxon_id ( sort { $placements{$qname}{$b} <=> $placements{$qname}{$a} } keys %{ $placements{$qname} } ) {
 			$placements{$qname}{$taxon_id} /= $placecount;
 			my ( $taxon_name, $taxon_level, $tid ) = get_taxon_info( taxon => $taxon_id );
+			$taxon_level = "Unknown" unless defined($taxon_level);
+			$taxon_name  = "Unknown" unless defined($taxon_name);
 			print SEQUENCETAXA "$qname\t$taxon_id\t$taxon_level\t$taxon_name\t" . $placements{$qname}{$taxon_id} . "\n";
 		}
 		my $readsummary = sum_taxon_levels( placements => $placements{$qname} );
 		foreach my $taxon_id ( sort { $readsummary->{$b} <=> $readsummary->{$a} } keys %{$readsummary} ) {
 			my ( $taxon_name, $taxon_level, $tid ) = get_taxon_info( taxon => $taxon_id );
+			$taxon_level = "Unknown" unless defined($taxon_level);
+			$taxon_name  = "Unknown" unless defined($taxon_name);
 			print SEQUENCESUMMARY "$qname\t$taxon_id\t$taxon_level\t$taxon_name\t" . $readsummary->{$taxon_id} . "\n";
 		}
 	}
