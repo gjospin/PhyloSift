78d7280ac546ef96a6a0540a4e02481d34c7247c
Eric Lowe <elowe@ucdavis.edu>
2012-08-15 19:44:18 -0700

    Fixed if statements

--- tools/testbench.sh
+++ tools/testbench.sh
@@ -20,25 +20,25 @@ ext=`echo $2 | sed 's/.*\.//'` # Gets file extension, for checking if .fastq or 
 test='fastq' # variable for testing file extension
 # fasta file to use; note that sim_data1 is where my file is located, can be changed as needed
 # .fa file is required in the directory for script to run properly
-file=sim_data1/`basename $2 .fastq`.fa  
 
 if [ -e $meganfile ] ; # if MEGAN has been ran today, do less work
 then
-    # gi_to_taxid lookup file must be supplied for proper usage.  Mine is located in /home/elowe/Testing
-    # but file can be anywhere.  Change as needed so that script finds gi_taxid_nucl.bin
-    echo -e "open file='$meganfile'\ncollapse rank=Species\nselect rank=Species\nexport what=CSV format=readname_taxonid separator=comma file='/home/elowe/$output'\nquit" > /home/elowe/meg_input.txt
+echo -e "open file='$meganfile'\ncollapse rank=Species\nselect rank=Species\nexport what=CSV format=readname_taxonid separator=comma file='/home/elowe/$output'\nquit" > /home/elowe/meg_input.txt
 else # MEGAN has not been run today, do long run
-    echo -e "load gi2taxfile='/home/elowe/Testing/gi_taxid_nucl.bin'\nimport blastfile='$1' fastafile='$file' meganfile='$meganfile'\ncollapse rank=Species\nselect rank=Species\nexport what=CSV format=readname_taxonid separator=comma file='/home/elowe/$output'\nquit" > /home/elowe/meg_input.txt 
-fi # end of if statement
+    if [ $ext == $test ] ; # if file extension is .fastq
+    then
+        file=sim_data1/`basename $2 .fastq`.fa
+        # gi_to_taxid lookup file must be supplied for proper usage.  Mine is located in /home/elowe/Testing
+        # but file can be anywhere.  Change as needed so that script finds gi_taxid_nucl.bin
+        echo -e "load gi2taxfile='/home/elowe/Testing/gi_taxid_nucl.bin'\nimport blastfile='$1' fastafile='$file' meganfile='$meganfile'\ncollapse rank=Species\nselect rank=Species\nexport what=CSV format=readname_taxonid separator=comma file='/home/elowe/$output'\nquit" > /home/elowe/meg_input.txt
+    else
+        file=$dirname
+        echo -e "load gi2taxfile='/home/elowe/Testing/gi_taxid_nucl.bin'\nimport blastfile='$1' fastafile='$file' meganfile='$meganfile'\ncollapse rank=Species\nselect rank=Species\nexport what=CSV format=readname_taxonid separator=comma file='/home/elowe/$output'\nquit" > /home/elowe/meg_input.txt
+    fi # end of if file extension is .fastq   
+fi # end of if MEGAN has been run today
 
-if [ $ext == $test ] ; # if file extension is .fastq
-then
-    # Runs MEGAN from command line
-    MEGAN +g < meg_input.txt &&
-else
-    MEGAN +g < meg_input.txt &&
-fi # end of if statement
-
+# Runs MEGAN from command line
+MEGAN +g < meg_input.txt
 # Calls megan_to_ps.pl, which is a perl script (included in Phylosift/tools/) that converts
 # MEGAN output to be similar to Phylosift output so that Phylosift Benchmark can be used
 # to compare MEGAN to PS.  Output is saved to sequence_taxa.txt in appropriate dir in 
