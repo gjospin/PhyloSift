b8ce15f6e4e5a8664216b85bc8097cb30cf42a42
Aaron Darling <aarondarling@ucdavis.edu>
2012-10-01 22:39:18 -0700

    more fixes and refinements

--- lib/Phylosift/Command/build_marker.pm
+++ lib/Phylosift/Command/build_marker.pm
@@ -19,18 +19,19 @@ sub usage_desc { "build_marker %o" }
 sub options {
 	return (
 		[ "force|f",        "Overwrites a previous Phylosift run with the same file name"],
-		[ "alignment=s",    "A multiple sequence alignment of the gene family"],
+		[ "alignment=s",    "A multiple sequence alignment of the gene family", { required => 1 }],
 		[ "update-only",    "Generate an updated marker only, no new HMM"],
-		[ "reps_pd=i",      "Specify the minimum divergence between representative sequences", { default => 0.1 }], 
-		[ "tree_pd=i",      "Specify the minimum phylogenetic diversity in the reference tree", { default => 0.1 }],
+		[ "unaligned=s",    "Unaligned and unmasked sequences corresponding from the gene family"],
+		[ "reps_pd=f",      "Specify the minimum divergence between representative sequences", { default => 0.1 }], 
+		[ "tree_pd=f",      "Specify the minimum phylogenetic diversity in the reference tree", { default => 0.1 }],
 		[ "taxonmap=s",     "A file containing a mapping of sequence names to taxon IDs"],
 	);
 }
 
 sub validate {
 	my ($self, $opt, $args) = @_;
-	
 	$self->usage_error("build_marker requires an alignment") unless defined $opt->{alignment};
+	$self->usage_error("build_marker --update-only requires unaligned sequences given with --unaligned") if defined $opt->{update_only} && !defined $opt->{unaligned};
 }
 
 sub load_opt {
@@ -38,21 +39,19 @@ sub load_opt {
 	my $opt = $args{opt};
 	$Phylosift::Settings::force = $opt->{force};
 	$Phylosift::Settings::configuration = $opt->{config};
-	$Phylosift::Settings::keep_search = $opt->{keep_search};
 	$Phylosift::Settings::disable_update_check = $opt->{disable_updates};
 	$Phylosift::Settings::my_debug = $opt->{debug};
 
-	$Phylosift::Utilities::debuglevel = $Phylosift::Settings::my_debug || 0;	
+	$Phylosift::Utilities::debuglevel = $Phylosift::Settings::my_debug || 1;	
 }
 
 sub execute {
 	my ($self, $opt, $args) = @_;
-	load_opt{opt=>$opt};
+	load_opt(opt=>$opt);
 	Phylosift::Command::sanity_check();
 
 	my $ps = new Phylosift::Phylosift();
 	Phylosift::MarkerBuild::build_marker(self=>$ps, opt=>$opt, alignment=>$opt->{alignment}, force=>$opt->{force}, cutoff=>$opt->{reps_pd}, mapping=>$opt->{taxonmap});
-
 }
 
 1;
