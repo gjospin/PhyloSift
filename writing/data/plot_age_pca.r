1880f08e2c0247c23fc697b243e674613ae5b895
Aaron Darling <aarondarling@ucdavis.edu>
2012-10-17 03:04:30 -0700

    read ref-based OTU qiime data

--- writing/data/plot_age_pca.r
+++ writing/data/plot_age_pca.r
@@ -50,7 +50,8 @@ plot(phy16$V2,phy16$V4,pch=16,col=colors[logagesphy16+1],xlab="PC1 ",ylab="PC2 "
 text(phy16$V2,phy16$V4,phy16$V1, cex=0.5)
 dev.off()
 
-meta16s <- read.table("metagenome_16s_weighted_unifrac_even_pc.txt",header=T,sep="\t")
+
+meta16s <- read.table("metagenome_16s_weighted_unifrac_r2.txt",header=T,sep="\t")
 
 ll<-nrow(meta16s)-2
 
@@ -93,7 +94,7 @@ dev.off()
 pdf("allofem.pdf")
 par(mfrow=c(2,2))
 plot(-amp16s$X1[1:ll],amp16s$X2[1:ll],pch=16,col=colors[logages+1],xlab="PC1 ",ylab="PC2 ", main="QIIME on 16S rRNA amplicons")
-plot(meta16s$X1[1:ll],-meta16s$X3[1:ll],pch=16,col=colors[logagesqiimeta+1],xlab="PC1 ",ylab="PC3 ", main="QIIME on metagenomic 16S rRNA")
+plot(-meta16s$X1[1:ll],-meta16s$X2[1:ll],pch=16,col=colors[logagesqiimeta+1],xlab="PC1 (38.9%)",ylab="PC2 (17.9%)", main="QIIME on metagenomic 16S rRNA")
 plot(-phy16$V2,phy16$V3,pch=16,col=colors[logagesphy16+1],xlab="PC1 ",ylab="PC2 ", main="phylosift on metagenomic 16S rRNA")
 plot(phypro$V2,phypro$V3,pch=16,col=colors[logagesphy+1],xlab="PC1 (59.9%)",ylab="PC2 (15.2%)", main="phylosift on metagenomic proteins")
 
