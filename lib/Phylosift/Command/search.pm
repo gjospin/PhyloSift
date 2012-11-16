539d046d089dc6346780c2360c28ffa07d0cfe26
Guillaume Jospin <gjospin@ucdavis.edu>
2012-11-16 15:04:36 -0800

    Reinstated default value of 1 for --chunks for the individual step modes

--- lib/Phylosift/Command/search.pm
+++ lib/Phylosift/Command/search.pm
@@ -48,7 +48,9 @@ sub execute {
 	$ps = $ps->initialize( mode => "search", file_1 => @$args[0], file_2 => @$args[1] );
 	$ps->{"ARGV"} = \@ARGV;
 	Phylosift::Settings::set_default( parameter => \$Phylosift::Settings::start_chunk, value => 1 );
+	Phylosift::Settings::set_default( parameter => \$Phylosift::Settings::chunks, value => 1 );
 	$ps->run( force => $Phylosift::Settings::force, custom => $Phylosift::Settings::custom, cont => $Phylosift::Settings::continue );
+	
 }
 
 1;
