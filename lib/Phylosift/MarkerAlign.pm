d003d04b4258a93840cd4e5e07817617b1956930
Guillaume Jospin <gjospin@ucdavis.edu>
2013-09-17 15:46:53 -0700

    When restarting a run the check on the data type on Gzipped files was not being done correctly using ps_open instead of open_sequence_file

--- lib/Phylosift/MarkerAlign.pm
+++ lib/Phylosift/MarkerAlign.pm
@@ -524,8 +524,9 @@ sub alignAndMask {
 
 		my $type = $self->{readtype};
 		unless ( defined($type) ) {
-			my $reads_file = ps_open( $self->{"readsFile"} );
+			my $reads_file = Phylosift::Utilities::open_sequence_file( file => $self->{"readsFile"} );
 			$type = Phylosift::Utilities::get_sequence_input_type($reads_file) unless defined $type;
+			debug "TYPE2 $type->{seqtype}\t$type->{format}". $self->{"readsFile"}."\n";
 		}
 		if ( $type->{seqtype} ne "protein" && Phylosift::Utilities::is_protein_marker( marker => $marker ) ) {
 
